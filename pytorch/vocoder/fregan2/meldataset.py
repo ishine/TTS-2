3d8468b7de39fd4896d9a5113cc782ca12ec7aa9
tebin <17797919+tebin@users.noreply.github.com>
2022-05-05 06:06:33 +0900

    fregan

--- pytorch/vocoder/fregan2/meldataset.py
+++ pytorch/vocoder/fregan2/meldataset.py
@@ -56,9 +56,9 @@ def mel_spectrogram(y, n_fft, num_mels, sampling_rate, hop_size, win_size, fmin,
         mel_basis[str(fmax) + '_' + str(y.device)] = torch.from_numpy(mel).float().to(y.device)
         hann_window[str(y.device)] = torch.hann_window(win_size).to(y.device)
 
-    y = torch.nn.functional.pad(y.unsqueeze(1), (int((n_fft - hop_size) / 2), int((n_fft - hop_size) / 2)),
-                                mode='reflect')
-    y = y.squeeze(1)
+    if not center:
+        y = torch.nn.functional.pad(y.unsqueeze(1), (int((n_fft-hop_size)/2), int((n_fft-hop_size)/2)), mode='reflect')
+        y = y.squeeze(1)
 
     spec = torch.stft(y, n_fft, hop_length=hop_size, win_length=win_size, window=hann_window[str(y.device)],
                       center=center, pad_mode='reflect', normalized=False, onesided=True)
